c1a42448ee5f7531d56b5d9506698c75f79ab20d
Bhavya Vohra <bhavyavohra8@gmail.com>
2024-06-02 12:26:24 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -30,12 +30,16 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <ImageSearch searchText={text => setTerm(text)} /> 
-      <div className="grid grid-cols-3 gap-4">
-        {loading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : images.map(image => (
-          <ImageCard key={image.key} image={image} /> 
-        ))}
-      </div>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+      {loading ? (
+        <h1 className="text-xl font-semibold text-center">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.key} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
