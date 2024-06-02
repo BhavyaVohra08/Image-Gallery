cd1589ee03de3d4af5eb90663a4416ecc39ccb57
Bhavya Vohra <bhavyavohra8@gmail.com>
2024-06-02 12:05:22 +0530

    publish

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import ImageCard from "./components/ImageCard";
+import ImageSearch from "./components/ImageSearch";
+
+function App() {
+  const key = import.meta.env.REACT_APP_API_KEY;
+  const [images, setImages] = useState([]);
+  const [term, setTerm] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  async function fetchData() {
+    try {
+      const res = await fetch(
+        `https://pixabay.com/api/?key=${key}&q=${term}&image_type=photo&pretty=true`
+      );
+      const data = await res.json();
+      setImages(data.hits);
+      setLoading(false);
+      console.log(images);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [term]);
+
+  return (
+    <div className="container mx-auto">
+      <ImageSearch searchText={text => setTerm(text)} /> 
+      <div className="grid grid-cols-3 gap-4">
+        {loading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : images.map(image => (
+          <ImageCard key={image.key} image={image} /> 
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const ImageCard = ({ image }) => {
+  const tags = image.tags.split(",");
+  return (
+    <div className="max-w-sm rounded-lg shadow-xl overflow-hidden">
+      <img src={image.webformatURL} alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          Photo by {image.user}
+        </div>
+        <ul>
+          <li>
+            <strong>Views: </strong>
+            {image.views}
+          </li>
+          <li>
+            <strong>Downloads: </strong>
+            {image.downloads}
+          </li>
+          <li>
+            <strong>Likes: </strong>
+            {image.likes}
+          </li>
+        </ul>
+      </div>
+      <div className="px-6 py-4">
+        {tags.map((tag) => (
+          <span className="inline-block px-3 py-1 rounded-full bg-gray-200 text-sm font-semibold text-gray-700 mr-2">
+            #{tag}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ImageCard;
